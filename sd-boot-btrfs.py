67ead7fc30308c0f9c83820051dc0de76387def2
Marcos Mello <marcosfrm@gmail.com>
2026-09-23 17:44:13 -0300

    Tornar usável
    O código anterior era problemático pois os arquivos do kernel e initramfs na partição EFI (/boot) ficavam
    dessincronizados com o diretório raiz da instalação. O Linux não dá garantia de compatibilidade em sua ABI
    interna, mesmo entre minor versions; assim, os módulos de /usr/lib/modules/<kernel>/ dificilmente carregariam
    em uma versão diferente. O diretório /boot-backup mantinha uma cópia de /boot do momento em que cada snapshot
    era feito, mas para usá-la era necessário iniciar por uma mídia live e sobrescrevê-la manualmente em /boot.
    
    Agora, a cada snapshot, uma cópia do kernel e initramfs é criada na partição EFI e a configuração do
    systemd-boot aponta diretamente para esses arquivos isolados, resolvendo o problema. Essas cópias são
    rotacionadas junto com os snapshots Btrfs.
    
    O único cuidado é que múltiplas cópias são mantidas na partição EFI. Com a autodetecção de hardware, o par
    kernel + initramfs ocupa cerca de 50 MiB aqui. Para manter 4 snapshots, são necessários 200 MiB adicionais.
    Uma partição EFI entre 500 MiB e 1 GiB está de bom tamanho.

--- sd-boot-btrfs.py
+++ sd-boot-btrfs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import btrfsutil, os, psutil, re, signal, shutil, sys
+import btrfsutil, ctypes, os, psutil, re, signal, shutil, sys
 from contextlib import contextmanager
 from datetime import datetime
 from dasbus.connection import SystemMessageBus
@@ -10,11 +10,27 @@ from dasbus.unix import GLibClientUnix
 # =============================================================================
 ORIGEM, DESTINO, MANTER = '/', '/snapshots', 4
 CONFIG_ORIGEM = '/boot/loader/entries/arch-default.conf'
-GER_PKG = ['/usr/bin/yay', '/usr/bin/pacman']
-ESP_MNT, ESP_BKP = '/boot', '/boot-backup'
+GER_PKG = ('/usr/bin/yay', '/usr/bin/pacman')
+ESP_MNT = '/boot'
 
 # AUXILIARES
 # =============================================================================
+_libc = ctypes.CDLL('libc.so.6', use_errno=True)
+_libc.syncfs.argtypes = [ctypes.c_int]
+_libc.syncfs.restype = ctypes.c_int
+
+def syncfs(fd: int):
+    if _libc.syncfs(fd) != 0:
+        err = ctypes.get_errno()
+        raise OSError(err, os.strerror(err))
+
+def sync_esp():
+    d_fd = os.open(ESP_MNT, os.O_RDONLY | os.O_DIRECTORY)
+    try:
+        syncfs(d_fd)
+    finally:
+        os.close(d_fd)
+
 @contextmanager
 def inibe_desligamento():
     fd = None
@@ -35,20 +51,27 @@ def esperar_gerenciador_pacotes():
         print(f"Aguardando {len(procs)} processo(s) [{detalhes}]...", file=sys.stderr)
         psutil.wait_procs(procs)
 
-def backup_esp():
+def copiar_arquivos_boot(dir_boot):
     if not (os.path.ismount(ESP_MNT) and os.access(ESP_MNT, os.R_OK | os.X_OK)):
         raise RuntimeError(f"{ESP_MNT} está desmontado ou inacessível.")
 
-    if os.path.exists(ESP_BKP):
-        if os.path.ismount(ESP_BKP):
-            raise RuntimeError(f"{ESP_BKP} é um ponto de montagem!")
-        shutil.rmtree(ESP_BKP)
+    os.makedirs(dir_boot, exist_ok=True)
 
-    shutil.copytree(ESP_MNT, ESP_BKP)
-    os.sync()
+    with open(CONFIG_ORIGEM, 'r') as f:
+        linhas = f.readlines()
 
-def criar_entrada_boot(snap_nome, dir_conf):
-    snap_entrada = os.path.join(dir_conf, f"{snap_nome}.conf")
+    for l in linhas:
+        l = l.strip().split()
+        if l and l[0] in ('linux', 'initrd'):
+            src = os.path.join(ESP_MNT, l[1].lstrip('/'))
+            if os.path.exists(src):
+                # caminho da origem sempre em relação à ESP
+                # eventuais subdiretórios na origem são descartados no destino
+                shutil.copy2(src, os.path.join(dir_boot, os.path.basename(l[1])))
+
+    sync_esp()
+
+def criar_entrada_boot(snap_nome, ent_conf):
     # o ponto de montagem *atual* do destino difere do caminho do subvolume dentro do
     # sistema de arquivos em relação ao subvolume top-level
     subvol_path = os.path.join(btrfsutil.subvolume_path(DESTINO), snap_nome)
@@ -69,32 +92,39 @@ def criar_entrada_boot(snap_nome, dir_conf):
         elif l.startswith('options'):
             novo_subv = f"subvol={subvol_path}"
             nova_conf.append(f"{re.sub(r'subvol=[^\s]+', novo_subv, l)}\n")
+        elif l.startswith('linux') or l.startswith('initrd'):
+            l = l.split()
+            novo_arqv = os.path.join('/snapshots', snap_nome, os.path.basename(l[1]))
+            nova_conf.append(f"{l[0]:<9}{novo_arqv}\n")
         else:
             nova_conf.append(f"{l}\n")
 
-    with open(snap_entrada, 'w') as f:
+    with open(ent_conf, 'w') as f:
         f.writelines(nova_conf)
-        f.flush()
-        os.fsync(f.fileno())
+
+    sync_esp()
 
 def rotacionar_snapshots(snap_pref, dir_conf):
     with btrfsutil.SubvolumeIterator(DESTINO, info=True) as it:
         snaps = sorted([x for x in it if x[0].startswith(snap_pref)], key=lambda x: x[1].otime)
 
+    snap_mudou = False
     for path, _ in snaps[:-MANTER]:
         snap_remove = os.path.join(DESTINO, path)
         btrfsutil.delete_subvolume(snap_remove)
-        btrfsutil.sync(ORIGEM)
+        snap_mudou = True
 
         conf_remove = os.path.join(dir_conf, f"{path}.conf")
         if os.path.exists(conf_remove):
             os.remove(conf_remove)
 
-            d_fd = os.open(dir_conf, os.O_RDONLY | os.O_DIRECTORY)
-            try:
-                os.fsync(d_fd)
-            finally:
-                os.close(d_fd)
+        boot_remove = os.path.join(ESP_MNT, 'snapshots', path)
+        if os.path.exists(boot_remove):
+            shutil.rmtree(boot_remove)
+
+    if snap_mudou:
+        btrfsutil.sync(ORIGEM)
+        sync_esp()
 
 # LÓGICA PRINCIPAL
 # =============================================================================
@@ -109,26 +139,37 @@ def main():
     try:
         with inibe_desligamento():
             esperar_gerenciador_pacotes()
-            backup_esp()
 
             snap_base = ORIGEM.strip('/').replace('/', '-')
             snap_pref = f"@{snap_base}_"
             snap_nome = f"{snap_pref}{datetime.now().strftime('%Y%m%d_%H%M%S')}"
             snap_path = os.path.join(DESTINO, snap_nome)
+            dir_boot = os.path.join(ESP_MNT, 'snapshots', snap_nome)
             dir_conf = os.path.dirname(CONFIG_ORIGEM)
+            ent_conf = os.path.join(dir_conf, f"{snap_nome}.conf")
 
             btrfsutil.create_snapshot(ORIGEM, snap_path)
 
             try:
-                criar_entrada_boot(snap_nome, dir_conf)
+                copiar_arquivos_boot(dir_boot)
+                criar_entrada_boot(snap_nome, ent_conf)
                 status = 0
             except Exception as e:
                 print(f"Desfazendo snapshot {snap_nome}, erro ao criar entrada de inicialização: {e}", file=sys.stderr)
                 btrfsutil.delete_subvolume(snap_path)
                 btrfsutil.sync(ORIGEM)
+
+                if os.path.exists(dir_boot):
+                    shutil.rmtree(dir_boot)
+
+                if os.path.exists(ent_conf):
+                    os.remove(ent_conf)
+
+                sync_esp()
                 status = 1
 
-            rotacionar_snapshots(snap_pref, dir_conf)
+            if status == 0:
+                rotacionar_snapshots(snap_pref, dir_conf)
             return status
 
     except Exception as e:
